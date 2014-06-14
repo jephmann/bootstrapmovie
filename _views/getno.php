e2e134fbf113c38cf7a1037cc0aaadff221205fa
jephmann <jephmann@gmail.com>
2014-06-13 22:43:29 -0500

    spelling corrections

--- _views/getno.php
+++ _views/getno.php
@@ -1,7 +1,9 @@
 <div class="container">
     <p>
-        This is a view file I "swap" in case I have a link without a querstring parameter for a person or movie id.
-        Until I build searh forms, feel free to click on either of these links and then keep clicking for cross-references,
+        This is a view file I "swap" in case I have a link without a querystring
+        parameter for a person or movie id.
+        Until I build search forms, feel free to click on either of these links
+        and then keep clicking for cross-references.
     </p>
     <ul>
         <li><a href="title.php?id=11235">Local Hero</a></li>
